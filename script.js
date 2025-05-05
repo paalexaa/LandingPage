edc3da3d3614240e00e298a622c50e950498f0c7
pozdniakova <xpozdniakova@stuba.sk>
2025-05-05 15:24:58 +0200

    commit_4: redesin site

--- script.js
+++ script.js
@@ -6,4 +6,12 @@ document.addEventListener("DOMContentLoaded", function () {
       links.classList.toggle("show");
     });
   });
-  
+
+window.addEventListener('scroll', function () {
+  const header = document.querySelector('header');
+  if (window.scrollY > 50) {
+      header.classList.add('scrolled');
+  } else {
+      header.classList.remove('scrolled');
+  }
+});
